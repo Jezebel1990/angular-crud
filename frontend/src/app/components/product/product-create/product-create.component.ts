3cc729d6dae5bde6c1332b607ba0137a0f201ecb
Jezebel1990 <jezebelguedes@gmail.com>
2024-11-11 00:49:44 -0300

    feat: Create product

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from './../product.service';
 import { Router } from '@angular/router';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product-create',
@@ -8,6 +9,12 @@ import { Router } from '@angular/router';
   styleUrls: ['./product-create.component.css']
 })
 export class ProductCreateComponent implements OnInit {
+
+product: Product = {
+  name: '',
+  price: null
+}
+
 
   constructor(private productService: ProductService,
     private router: Router) { }
@@ -17,7 +24,18 @@ export class ProductCreateComponent implements OnInit {
 }
 
 createProduct(): void {
+ this.productService.create(this.product).subscribe((createdProduct) => {
+  if (typeof createdProduct.id === 'string') {
+    createdProduct.id = Number(parseInt(createdProduct.id, 16)); 
+  }
+  if (typeof createdProduct.price === 'string') {
+    createdProduct.price = parseFloat(createdProduct.price);
+  }
   this.productService.showMessage('Produto criado!')
+  this.router.navigate(['/products'])
+  }, err => {
+      this.productService.showMessage('Erro ao criar o produto');
+    });
   }
 
 cancel(): void {
--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -1,12 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Product } from './product.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor(private snackBar: MatSnackBar) { }
+  baseUrl = "https://angular-crud-api.onrender.com/products"
+
+  constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
 
   showMessage(msg: string): void {
     this.snackBar.open(msg, 'X', {
@@ -14,5 +19,8 @@ export class ProductService {
       horizontalPosition: "right",
       verticalPosition: "top"
     })
+  }
+  create(product: Product): Observable<Product> {
+    return this.http.post<Product>(this.baseUrl, product)
   }
 }
